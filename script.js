99ce4ba2ce3d89cbec4df0471dcbccb1eb9fb376
ksahh <haseebshaikh25ks@gmail.com>
2023-08-01 08:57:12 +0530

    Page 1 complete

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 const cursor = document.querySelector(".cursor")
 const blurCursor = document.querySelector(".blurCursor")
 
-document.addEventListener("mousemove" , dits => {
-    cursor.style.left = dits.x - 12.5 + "px"
-    cursor.style.top = dits.y - 12.5 + "px"
-    blurCursor.style.left = dits.x - 200 + "px"
-    blurCursor.style.top = dits.y - 200 + "px"
+document.addEventListener("mousemove", dits => {
+    setTimeout(() => {
+        cursor.style.left = dits.x - 12.5 + "px"
+        cursor.style.top = dits.y - 12.5 + "px"
+    }, 150);
+
+    setTimeout(() => {
+        blurCursor.style.left = dits.x - 250 + "px"
+        blurCursor.style.top = dits.y - 250 + "px"
+        blurCursor.style.opacity = 1;
+    }, 300);
 })
 
-gsap.to(".nav" , {
-    backgroundColor : "#000",
-    duration : 0.5,
-    height : "110px",
-    scrollTrigger : {
-        trigger : ".nav",
-        markers : true,
-        start : "top -10%",
-        end : "top -11%",
-        scrub : 1
+
+gsap.to(".nav", {
+    backgroundColor: "#000",
+    duration: 0.5,
+    height: "110px",
+    scrollTrigger: {
+        trigger: ".nav",
+        markers: true,
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 1
     }
 })
 
-gsap.to(".main" , {
-    backgroundColor : "#000",
-    scrollTrigger : {
-        trigger : "main",
-        markers : true,
-        start : "top -25%",
-        end : "top -75%",
-        scrub : 1
+gsap.to(".main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "main",
+        markers: true,
+        start: "top -25%",
+        end: "top -75%",
+        scrub: 1
     }
 })
