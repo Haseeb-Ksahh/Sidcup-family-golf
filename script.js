f226b633b622abef6aed0999022d5dc9c9f43698
ksahh <haseebshaikh25ks@gmail.com>
2023-08-03 17:15:14 +0530

    added new section

--- script.js
+++ script.js
@@ -2,13 +2,26 @@ const cursor = document.querySelector(".cursor")
 const blurCursor = document.querySelector(".blurCursor")
 
 document.addEventListener("mousemove", dits => {
-
-        cursor.style.left = dits.x + "px"
-        cursor.style.top = dits.y + "px"
-        blurCursor.style.left = dits.x - 250 + "px"
-        blurCursor.style.top = dits.y - 250 + "px"
+    cursor.style.left = dits.x + "px"
+    cursor.style.top = dits.y + "px"
+    blurCursor.style.left = dits.x - 250 + "px"
+    blurCursor.style.top = dits.y - 250 + "px"
 })
 
+const allh4 = document.querySelectorAll(".cursorhover")
+
+allh4.forEach(element => {
+    element.addEventListener("mouseenter", function () {
+        cursor.style.scale = 3
+        cursor.style.border = "1px solid #fff"
+        cursor.style.backgroundColor = "transparent"
+    })
+    element.addEventListener("mouseleave", function () {
+        cursor.style.scale = 1
+        cursor.style.border = "0px solid #fff"
+        cursor.style.backgroundColor = "#95C11E"
+    })
+})
 
 gsap.to(".nav", {
     backgroundColor: "#000",
@@ -31,5 +44,29 @@ gsap.to(".main", {
         start: "top -25%",
         end: "top -75%",
         scrub: 1
+    }
+})
+
+gsap.to("#colon1" , {
+    x : 55,
+    y : 55,
+    scrollTrigger : {
+        trigger : "#colon1",
+        markers : true,
+        start : "top 70%",
+        end : "top 45%",
+        scrub : 3
+    }
+})
+
+gsap.to("#colon2" , {
+    x : -55,
+    y : -55,
+    scrollTrigger : {
+        trigger : "#colon1",
+        markers : true,
+        start : "top 70%",
+        end : "top 45%",
+        scrub : 3
     }
 })
